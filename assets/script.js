d285dfdb0fa5cbc78bed8b22550bd70396ffcef7
Tatum Sterling <tatum.sterling29@gmail.com>
2023-09-26 23:08:43 -0500

    fetched info from api for city, humidity, temp, and wind

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,66 @@
 var searchBtn = $("#search-btn");
 var searchHistory= $("#search-hist");
+var userInput = $("#search-bar");
 
 
 
 searchBtn.on("click", searchCity);
 
 function searchCity() {
-    var userInput = $("#search-bar").val();
-    localStorage.setItem("city", userInput);
+    var citySearch = userInput.val();
+    console.log("user input", citySearch);
+    localStorage.setItem("city", citySearch);
     // need to figure out how to save multiple searches
     // under different key terms
+    convertToCoord(citySearch);
+    
 }
 
 function renderLastCity() {
     // need to create a list item
     // append it to the ul("#city-list")
-    // get from local storage
+    // get from local storageoko
     // inner HTML is local storage city value
 
 }
 
 //TODO need to pull in a weather API to access 
 // weather from different cities
+
+var convertToCoord = function (city) {
+    console.log("parameter", city)
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=4b6fcde070c0b9b25bca1edc64948080";
+
+    fetch(apiUrl).then(function(result) {
+        if (result.ok) {
+            result.json().then(function(data) {
+                console.log('data', data)
+                var searchLat = data[0].lat;
+                var searchLon = data[0].lon;
+                console.log(searchLat);
+                getWeather(searchLat, searchLon);
+            });
+        }
+    })
+}
+    
+
+
+var getWeather = function(latitude, longitude) {
+    console.log(latitude, longitude);
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude+"&lon="+ longitude +"&appid=4b6fcde070c0b9b25bca1edc64948080";
+
+    fetch(apiUrl).then(function(result) {
+        if (result.ok) {
+            result.json().then(function(data) {
+                console.log("data", data);
+                var humidity =  data.list[0].main.humidity;
+                var temp = data.list[0].main.temp;
+                var wind = data.list[0].wind.speed;
+
+                console.log("wind speed", data.list[0].wind.speed);
+            })
+        }
+    })
+
+}
