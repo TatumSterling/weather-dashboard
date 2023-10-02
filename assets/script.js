7b76a86db26157c0e13ee8ffd4ca645a4f0c1858
Tatum Sterling <tatum.sterling29@gmail.com>
2023-10-02 13:50:55 -0500

    added search history javascript and styling, fixed bugs in forecast cards

--- assets/script.js
+++ assets/script.js
@@ -10,29 +10,53 @@ var futureForecastEL = $("#future-container");
 var tempElMain = $("#temperature");
 var humidityElMain = $("#humidity");
 var windElMain = $("#wind-speed");
+var searchCounter = 0;
 
 
 
-
-searchBtn.on("click", searchCity);
-
-function searchCity() {
+// Add a click event listener to the search button
+searchBtn.on("click", function() {
+    // Get the user's search input
     var citySearch = userInput.val();
+    
+    // Generate a unique key for this search (e.g., using a counter)
+    var searchKey = "search_" + searchCounter;
+    
+    // Save the search data to local storage with the unique key
+    localStorage.setItem(searchKey, citySearch);
+    
+
+    searchCounter++;
+
     console.log("user input", citySearch);
-    localStorage.setItem("city", citySearch);
-    // need to figure out how to save multiple searches
-    // under different key terms
+    console.log("Saved with key", searchKey);
+
     convertToCoord(citySearch);
 
-}
+});
 
 function renderLastCity() {
-    // need to create a list item
-    // append it to the ul("#city-list")
-    // get from local storageoko
-    // inner HTML is local storage city value
+    // Clear the existing search history if any
+    $("#search-hist").empty(); 
 
+    // Iterate through local storage to retrieve search data
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var value = localStorage.getItem(key);
+
+        // Check if the key starts with "search_" to filter search data
+        if (key.startsWith("search_")) {
+            // Create a button to display the search
+            var listItem = $("<button>").text(value);
+            listItem.addClass("city-history");
+
+            // Append the button to the search history
+            $("#search-hist").append(listItem); 
+        }
+    }
 }
+
+
 
 
 
@@ -64,6 +88,7 @@ var getWeather = function (latitude, longitude) {
             result.json().then(function (data) {
                 console.log("data", data);
 
+
                 var condition = data.list[0].weather[0].description;
                 var icon = data.list[0].weather[0].icon;
                 var humid = data.list[0].main.humidity;
@@ -73,9 +98,9 @@ var getWeather = function (latitude, longitude) {
 
 
                 var weatherList = [
-                    ["Temperature: ", temp, "F"],
+                    ["Temperature: ", temp, "°F"],
                     ["Humidity: ", humid, "%"],
-                    ["Wind Speed: ", wind, "MPH"]
+                    ["Wind Speed: ", wind, "mph"]
                 ]
 
                 console.log(weatherList);
@@ -91,12 +116,28 @@ var getWeather = function (latitude, longitude) {
                 var iconUrl = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
                 iconMain.attr("src", iconUrl);
 
+                var today = dayjs();
+
+                // Display the current date
+                $('#todays-date').text(today.format('dddd, MMMM D YYYY'));
+                
+                // Display the dates for the next 5 days
+                for (var i = 1; i <= 5; i++) {
+                    var nextDay = today.add(i, 'day'); // Add 'i' days to the current date
+                    var dateElementId = '#future-date-' + i; 
+                    
+                    // Display the formatted date in the corresponding element
+                    $(dateElementId).text(nextDay.format('dddd, MMMM D YYYY'));
+                }
+
                 for (var i = 7; i < 36; i += 7) {
-                    // Construct the IDs for the HTML elements
+
                     var dateId = "future-date-" + (i / 7);
                     var iconId = "future-icon-" + (i / 7);
                     var tempId = "future-temp-" + (i / 7);
                     var humidId = "future-humid-" + (i / 7);
+                    var windId = "future-wind-" + (i / 7);
+                    var conditionId = "future-condition-" + (i / 7);
                   
                     // Get the data for this iteration
                     var conditionFuture = data.list[i].weather[0].description;
@@ -104,27 +145,18 @@ var getWeather = function (latitude, longitude) {
                     var humidFuture = data.list[i].main.humidity;
                     var tempFuture = data.list[i].main.temp;
                     var windFuture = data.list[i].wind.speed;
+        
                   
                     // Update the HTML elements with the data
-                    document.getElementById(dateId).textContent = conditionFuture;
+                    document.getElementById(conditionId).textContent = conditionFuture;
                     var futureIconUrl = "https://openweathermap.org/img/wn/"+ iconFuture +"@2x.png"
                     document.getElementById(iconId).setAttribute("src", futureIconUrl);
-                    // document.getElementById(iconId).src = iconFuture;
-                    document.getElementById(tempId).textContent ="temp: "+ tempFuture, "F";
+                    document.getElementById(tempId).textContent ="temp: "+ tempFuture+ "°F";
                     document.getElementById(humidId).textContent ="humidity: "+ humidFuture+ "%";
+                    document.getElementById(windId).textContent= "wind speed: " +windFuture+ "mph";
 
-                // for (var i =7; i<36; i+=7) {
-                    
-                // var conditionFuture = data.list[i].weather[0].description;
-                // var iconFuture = data.list[i].weather[0].icon;
-                // var humidFuture = data.list[i].main.humidity;
-                // var tempFuture = data.list[i].main.temp;
-                // var windFuture = data.list[i].wind.speed;
-
-                // console.log(conditionFuture);
-
-
-
+                    $("#page-container").css("display", "block")
+                    renderLastCity();
 
                 }
 
@@ -136,7 +168,5 @@ var getWeather = function (latitude, longitude) {
         }
     })
 
-    var today = dayjs();
-    $('#todays-date').text(today.format('dddd, MMMM D YYYY'));
 
 }
