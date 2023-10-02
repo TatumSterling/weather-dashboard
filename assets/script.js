2d0ea3b34b617e354b3f0697b4ef144cd480eb1f
Tatum Sterling <tatum.sterling29@gmail.com>
2023-10-02 15:14:55 -0500

    added finsihing touches in all files

--- assets/script.js
+++ assets/script.js
@@ -15,16 +15,16 @@ var searchCounter = 0;
 
 
 // Add a click event listener to the search button
-searchBtn.on("click", function() {
+searchBtn.on("click", function () {
     // Get the user's search input
     var citySearch = userInput.val();
-    
-    // Generate a unique key for this search (e.g., using a counter)
+
+    // Generate a unique key for this search
     var searchKey = "search_" + searchCounter;
-    
+
     // Save the search data to local storage with the unique key
     localStorage.setItem(searchKey, citySearch);
-    
+
 
     searchCounter++;
 
@@ -37,7 +37,7 @@ searchBtn.on("click", function() {
 
 function renderLastCity() {
     // Clear the existing search history if any
-    $("#search-hist").empty(); 
+    $("#search-hist").empty();
 
     // Iterate through local storage to retrieve search data
     for (var i = 0; i < localStorage.length; i++) {
@@ -50,14 +50,15 @@ function renderLastCity() {
             var listItem = $("<button>").text(value);
             listItem.addClass("city-history");
 
-            (function(cityName) {
-                listItem.on("click", function() {
+            //after you click on the button, it will display that city's weather
+            (function (cityName) {
+                listItem.on("click", function () {
                     convertToCoord(cityName);
                 });
             })(value);
 
             // Append the button to the search history
-            $("#search-hist").append(listItem); 
+            $("#search-hist").append(listItem);
         }
     }
 }
@@ -65,7 +66,7 @@ function renderLastCity() {
 
 
 
-
+// putting it all together, fetching the data from the weather api
 var convertToCoord = function (city) {
     console.log("parameter", city)
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=4b6fcde070c0b9b25bca1edc64948080";
@@ -94,7 +95,7 @@ var getWeather = function (latitude, longitude) {
             result.json().then(function (data) {
                 console.log("data", data);
 
-
+                // getting handles on data I need from api
                 var condition = data.list[0].weather[0].description;
                 var icon = data.list[0].weather[0].icon;
                 var humid = data.list[0].main.humidity;
@@ -102,64 +103,59 @@ var getWeather = function (latitude, longitude) {
                 var wind = data.list[0].wind.speed;
                 var cityName = data.city.name;
 
-
-                var weatherList = [
-                    ["Temperature: ", temp, "°F"],
-                    ["Humidity: ", humid, "%"],
-                    ["Wind Speed: ", wind, "mph"]
-                ]
-
-                console.log(weatherList);
-
-                tempElMain.html(weatherList[0]);
-                humidityElMain.html(weatherList[1]);
-                windElMain.html(weatherList[2]);
+                // adding html content for current weather card
+                tempElMain.html("Temperature: ", temp, "°F");
+                humidityElMain.html("Humidity: ", humid, "%");
+                windElMain.html("Wind Speed: ", wind, "mph");
 
 
                 $(cityNameEl).html(cityName);
                 $(descriptionMain).html(condition);
 
-                var iconUrl = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+                var iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
                 iconMain.attr("src", iconUrl);
 
                 var today = dayjs();
 
                 // Display the current date
                 $('#todays-date').text(today.format('dddd, MMMM D YYYY'));
-                
+
                 // Display the dates for the next 5 days
                 for (var i = 1; i <= 5; i++) {
-                    var nextDay = today.add(i, 'day'); // Add 'i' days to the current date
-                    var dateElementId = '#future-date-' + i; 
-                    
-                    // Display the formatted date in the corresponding element
+                    var nextDay = today.add(i, 'day');
+                    //gets a handle on the future-date id tags
+                    var dateElementId = '#future-date-' + i;
+
+                    // Display the formatted date in each of the cards
                     $(dateElementId).text(nextDay.format('dddd, MMMM D YYYY'));
                 }
-
+                // loops through the list array from the api and chooses next 5
+                // days from list
                 for (var i = 7; i < 36; i += 7) {
 
-                    var dateId = "future-date-" + (i / 7);
-                    var iconId = "future-icon-" + (i / 7);
-                    var tempId = "future-temp-" + (i / 7);
-                    var humidId = "future-humid-" + (i / 7);
-                    var windId = "future-wind-" + (i / 7);
-                    var conditionId = "future-condition-" + (i / 7);
-                  
+                    var iconId = "#future-icon-" + (i / 7);
+                    var tempId = "#future-temp-" + (i / 7);
+                    var humidId = "#future-humid-" + (i / 7);
+                    var windId = "#future-wind-" + (i / 7);
+                    var conditionId = "#future-condition-" + (i / 7);
+
                     // Get the data for this iteration
                     var conditionFuture = data.list[i].weather[0].description;
                     var iconFuture = data.list[i].weather[0].icon;
                     var humidFuture = data.list[i].main.humidity;
                     var tempFuture = data.list[i].main.temp;
                     var windFuture = data.list[i].wind.speed;
-        
-                  
+
+
                     // Update the HTML elements with the data
-                    document.getElementById(conditionId).textContent = conditionFuture;
-                    var futureIconUrl = "https://openweathermap.org/img/wn/"+ iconFuture +"@2x.png"
-                    document.getElementById(iconId).setAttribute("src", futureIconUrl);
-                    document.getElementById(tempId).textContent ="temp: "+ tempFuture+ "°F";
-                    document.getElementById(humidId).textContent ="humidity: "+ humidFuture+ "%";
-                    document.getElementById(windId).textContent= "wind speed: " +windFuture+ "mph";
+
+                    $(conditionId).text(conditionFuture);
+
+                    var futureIconUrl = "https://openweathermap.org/img/wn/" + iconFuture + "@2x.png";
+                    $(iconId).attr("src", futureIconUrl);
+                    $(tempId).text("temp: " + tempFuture + "°F");
+                    $(humidId).text("humidity: " + humidFuture + "%");
+                    $(windId).text("wind speed: " + windFuture + "mph");
 
                     $("#page-container").css("display", "block")
                     renderLastCity();
