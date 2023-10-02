dd7513be74f11b73bb976af9851bdbf2335ecae7
Tatum Sterling <tatum.sterling29@gmail.com>
2023-10-02 09:50:55 -0500

    added content to current weather card

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 var searchBtn = $("#search-btn");
-var searchHistory= $("#search-hist");
+var searchHistory = $("#search-hist");
 var userInput = $("#search-bar");
+var cityNameEl = $("#current-city");
+var today = $("#todays-date");
+var twhMain = $("#temp-wind-humid");
+var descriptionMain = $("#description");
+var iconMain = $("#icon");
+var futureForecastEL = $("#future-container");
+var tempElMain = $("#temperature");
+var humidityElMain = $("#humidity");
+var windElMain = $("#wind-speed");
+
 
 
 
@@ -13,7 +23,7 @@ function searchCity() {
     // need to figure out how to save multiple searches
     // under different key terms
     convertToCoord(citySearch);
-    
+
 }
 
 function renderLastCity() {
@@ -24,16 +34,15 @@ function renderLastCity() {
 
 }
 
-//TODO need to pull in a weather API to access 
-// weather from different cities
+
 
 var convertToCoord = function (city) {
     console.log("parameter", city)
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=4b6fcde070c0b9b25bca1edc64948080";
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=4b6fcde070c0b9b25bca1edc64948080";
 
-    fetch(apiUrl).then(function(result) {
+    fetch(apiUrl).then(function (result) {
         if (result.ok) {
-            result.json().then(function(data) {
+            result.json().then(function (data) {
                 console.log('data', data)
                 var searchLat = data[0].lat;
                 var searchLon = data[0].lon;
@@ -43,24 +52,69 @@ var convertToCoord = function (city) {
         }
     })
 }
-    
 
 
-var getWeather = function(latitude, longitude) {
+
+var getWeather = function (latitude, longitude) {
     console.log(latitude, longitude);
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude+"&lon="+ longitude +"&appid=4b6fcde070c0b9b25bca1edc64948080";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=4b6fcde070c0b9b25bca1edc64948080&units=imperial";
 
-    fetch(apiUrl).then(function(result) {
+    fetch(apiUrl).then(function (result) {
         if (result.ok) {
-            result.json().then(function(data) {
+            result.json().then(function (data) {
                 console.log("data", data);
-                var humidity =  data.list[0].main.humidity;
+
+                var condition = data.list[0].weather[0].description;
+                var icon = data.list[0].weather[0].icon;
+                var humid = data.list[0].main.humidity;
                 var temp = data.list[0].main.temp;
                 var wind = data.list[0].wind.speed;
+                var cityName = data.city.name;
 
-                console.log("wind speed", data.list[0].wind.speed);
-            })
+
+                var weatherList = [
+                    ["Temperature: ", temp, "F"],
+                    ["Humidity: ", humid, "%"],
+                    ["Wind Speed: ", wind, "MPH"]
+                ]
+
+                console.log(weatherList);
+
+                tempElMain.html(weatherList[0]);
+                humidityElMain.html(weatherList[1]);
+                windElMain.html(weatherList[2]);
+
+
+                $(cityNameEl).html(cityName);
+                $(descriptionMain).html(condition);
+
+                var iconUrl = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+                iconMain.attr("src", iconUrl);
+
+                for (var i =7; i<36; i+=7) {
+                    
+                var conditionFuture = data.list[i].weather[0].description;
+                var iconFuture = data.list[i].weather[0].icon;
+                var humidFuture = data.list[i].main.humidity;
+                var tempFuture = data.list[i].main.temp;
+                var windFuture = data.list[i].wind.speed;
+
+                console.log(conditionFuture);
+
+
+
+
+                }
+
+            }
+
+
+
+            )
         }
     })
+
+    var today = dayjs();
+    $('#todays-date').text(today.format('dddd, MMMM D YYYY'));
 
 }
